e148d746e353fa833827cc91a5eb58bfbf74ade7
Jainil19 <jainilmaheshwari2000@gmail.com>
2022-06-13 13:49:28 +0530

    added greetings

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,56 +1,61 @@
 const asyncHandler = require('express-async-handler');
-const Bank = require('../models/goalModel')
+const Bank = require('../models/goalModel');
 
-const getGoals = asyncHandler(async (req,res) => {
-    const banks = await Bank.find()
-    res.status(200).json(banks)
-})
+const getGoals = asyncHandler(async (req, res) => {
+  const date = new Date();
+  let hours = date.getHours();
+  let status =
+    hours < 12 ? 'Morning' : hours <= 18 && hours >= 12 ? 'Afternoon' : 'Night';
+  res.send(status);
+  const banks = await Bank.find();
+  res.status(200).json({ greeting: status, banks: banks });
+});
 
-const setGoal = asyncHandler(async (req,res) => {
-    if(!req.body.name, !req.body.email, !req.body.status){
-        res.status(400)
-        throw new Error('please add a text')
-    }
-    
-const date = new Date;
-let hours = date.getHours();
-let status = (hours < 12)? "Morning" :
-             ((hours <= 18 && hours >= 12 ) ? "Afternoon" : "Night");
-res.send(status);
-    
-    const bank = await Bank.create({
-        name: req.body.name,
-        email: req.body.email,
-        status: req.body.status,
-    })
-    res.status(200).json(bank)
-})
+const setGoal = asyncHandler(async (req, res) => {
+  if ((!req.body.name, !req.body.email, !req.body.status)) {
+    res.status(400);
+    throw new Error('please add a text');
+  }
 
-const updateGoal =asyncHandler(async (req,res) => {
-    const bank = await Bank.findById(req.params.id)
-    if(!bank){
-        res.status(400)
-        throw new Error('bank not found')
-    }
-    const updateGoal = await Bank.findByIdAndUpdate(req.params.id, req.body,{
-        new:true,
-    })
-    res.status(200).json(updateGoal)
-})
+  const date = new Date();
+  let hours = date.getHours();
+  let status =
+    hours < 12 ? 'Morning' : hours <= 18 && hours >= 12 ? 'Afternoon' : 'Night';
+  res.send(status);
 
-const deleteGoal =asyncHandler(async (req,res) => {
-    const bank = await Bank.findById(req.params.id)
-    if(!bank){
-        res.status(400)
-        throw new Error('bank not found')
-    }
-    await bank.remove()
-    res.status(200).json({ id: req.params.id })
-})
+  const bank = await Bank.create({
+    name: req.body.name,
+    email: req.body.email,
+    status: req.body.status,
+  });
+  res.status(200).json({ greeting: status, bank: bank });
+});
+
+const updateGoal = asyncHandler(async (req, res) => {
+  const bank = await Bank.findById(req.params.id);
+  if (!bank) {
+    res.status(400);
+    throw new Error('bank not found');
+  }
+  const updateGoal = await Bank.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updateGoal);
+});
+
+const deleteGoal = asyncHandler(async (req, res) => {
+  const bank = await Bank.findById(req.params.id);
+  if (!bank) {
+    res.status(400);
+    throw new Error('bank not found');
+  }
+  await bank.remove();
+  res.status(200).json({ id: req.params.id });
+});
 
 module.exports = {
-    getGoals,
-    setGoal,
-    updateGoal,
-    deleteGoal
-}
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
